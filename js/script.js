9ca3316f31dcbc6d71e8d4cb89297b5282ebd2a5
Marcaj <marcaj13@gmail.com>
2019-11-04 08:49:40 +0100

    Add css animations to boxes

--- js/script.js
+++ js/script.js
@@ -59,3 +59,21 @@
     
     init();
 })();
+
+(function () {
+    const toAnimate = document.querySelectorAll(".animate");
+
+    let animationObserver = new IntersectionObserver(function (entries) {
+        console.log(entries);
+        entries.forEach(function (entry) {
+            if (entry.intersectionRatio > 0) {
+                entry.target.classList.remove("animate");
+                animationObserver.unobserve(entry.target);
+            }
+        });
+    });
+    
+    toAnimate.forEach(function (element) {
+        animationObserver.observe(element);
+    });
+})();
